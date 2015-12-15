110574cad8000d2bf3841355f0c8afcbbd760a86
Sebastian <sebastian.sanabria@brm.com.co>
2015-12-15 16:45:23 -0500

    Update redes.php

--- redes.php
+++ redes.php
@@ -1,4 +1,12 @@
 <?php
-require("db/requires.php"); 
+require("db/requires.php");
+
+if(isset($_COOKIE['idBrand']) && is_numeric($_COOKIE['idBrand'])){
+	//BUSCO LA MARCA EN LA BASE DE DATOS
+	$General= new General();
+	$marca=$General->getInstanciaWhere("MpBrand",'','idBrand='.$_COOKIE['idBrand']);
+	$nombreMarca=$marca[0]->name;
+}
+$smarty->assign('nombreMarca',$nombreMarca);
 $smarty->display("redes.html");
 ?>
