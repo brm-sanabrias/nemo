f6501f1d49eaafb543ada152c6b853f8ff74bbf1
Cristian Cortés <cristian.cortes@brm.com.co>
2015-12-15 09:45:26 -0500

    first branch commit .|.

--- buscar.php
+++
@@ -1,7 +0,0 @@
-<?php
-require("db/requires.php"); 
-$General= new General();
-
-$todasLasMarcas=$General->getInstanciaWhere("MpBrand");
-echo json_encode($todasLasMarcas);
-?>
--- db/requires.ini.php
+++
@@ -1,6 +0,0 @@
-<?php
-require("./".$prefijo."db/db.mpBrand.php");
-/*require("./".$prefijo."db/db.brmCargo.php");
-require("./".$prefijo."db/db.brmDimesion.php");
-require("./".$prefijo."db/db.brmEmpleado.php");*/
-?>
